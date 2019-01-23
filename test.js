3cc9702fdaa4613a54c93b96fe1759eae8549afb
dahalprastut <dahalprastut@gmail.com>
2019-01-23 15:53:58 +0545

    changes

--- test.js
+++ test.js
@@ -1,10 +1,37 @@
-var pricePhone=5500;
-var bankBalance = prompt('enter your bank banlance');
-if(bankBalance >= pricePhone){
-    alert('you can buy the phone');
-}
-else{
-    alert('you cannot afford it');
+// var number = prompt('enter any number');
+// number = Number(number);
+
+// if(Number.isNaN(number)===false && typeof number === "number"){
+//     alert('it is a number');
+// }
+// else{
+//     alert('is not a number');
+// }
+
+
+
+var phonePrice = 6000;
+var bankBalance = prompt('enter your bank balance');
+number= Number(bankBalance);
+// console.log(typeof bankBalance);
+// console.log(typeof number);
+if(isNaN(number)===true){
+    var bankBalance = prompt('that is not a number.please enter a number');
+    if(bankBalance >= phonePrice){
+        alert('you can buy the phone');
+    }
+    else{
+        alert('you cannot buy the phone');
+    }
 }
 
+else{
+    if(bankBalance >= phonePrice){
+        alert('you can buy the phone');
+    }
+    else{
+        alert('you cannot buy the phone');
+    }
+    sadfasdf
+}
 
