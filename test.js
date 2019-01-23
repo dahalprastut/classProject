1603882a279d8db2d39dbee7dae81ce95ace03e1
dahalprastut <dahalprastut@gmail.com>
2019-01-23 14:54:52 +0545

    frst commit

---
+++ test.js
@@ -0,0 +1,10 @@
+var pricePhone=5500;
+var bankBalance = prompt('enter your bank banlance');
+if(bankBalance >= pricePhone){
+    alert('you can buy the phone');
+}
+else{
+    alert('you cannot afford it');
+}
+
+
