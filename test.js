6684730bb15dcc533c6b5f8143c9ede2a411f8a4
dahalprastut <dahalprastut@gmail.com>
2019-01-24 16:43:53 +0545

    functions

--- test.js
+++ test.js
@@ -33,6 +33,8 @@
 //     } 
 // }
 
+// first task
+
 
 // var firstName = prompt('enter first name');
 // var lastName = prompt('enter last name');
@@ -46,18 +48,135 @@
 // }
 
 
-var firstName = prompt('enter first name');
-var lastName = prompt('enter last name');
-var age = prompt('enter the age');
-var gender  = prompt('enter the gender');
-if(age >= 18 && gender=='female'){
-    alert('eligible for all female college scholarship');
+// second task
+
+// var firstName = prompt('enter first name');
+// var lastName = prompt('enter last name');
+// var age = prompt('enter the age');
+// var gender  = prompt('enter the gender');
+// if(age >= 18 && gender=='female'){
+//     alert('eligible for all female college scholarship');
+// }
+// else if(age < 18 && gender=='female'){
+//     alert('eligible for girls school scholarship');
+// }
+// else if(age < 18 && gender=='male'){
+//     alert('not eligible scholarship');
+// }
+
+// function yearOfBirth(year){
+//     return 2017-year;
+// }
+
+// function retirement(name , age){
+//     var retirement = 65- yearOfBirth(age);
+//     console.log('name retires in ' + retirement + ' years');
+// }
+// retirement('john' , 2011);
+
+
+// var tip;
+// function calculator(amount){
+//     if(amount < 50){
+//       return tip=0.2*amount; 
+//     } else if (amount >= 50 && amount < 200){
+//        return tip = 0.15 * amount;
+//     }
+//     else{
+//        return tip=0.1*amount;
+//     }
+// }
+// var list1 = [calculator(124) , calculator(48) , calculator(268)];
+// console.log(list1);
+// var list2=[calculator(124)+124 , calculator(48)+48 , calculator(268)+268];
+// console.log(list2);
+
+
+// var john = {
+//     firstName:'john',
+//     lastName:'prasad',
+//     family:['mira','hira','harihar'],
+//     birthYear:1990,
+//     age:function(){
+//         this.year = 2019-this.birthYear;
+//     }
+    
+// };
+// john.age();
+// console.log(john);
+
+// var a= prompt("enter first number");
+// var b= prompt("enter second number");
+// firstNumber= Number(a);
+// secondNumber=Number(b);
+
+// function sum(firstNumber , secondNumber){
+//     return firstNumber + secondNumber;
+// }
+
+// alert(`sum is ${sum(firstNumber , secondNumber)}`);
+
+
+// function substract(firstNumber , secondNumber){
+    
+//    return firstNumber - secondNumber;
+// }
+// alert(`difference is ${substract(firstNumber , secondNumber)}`);
+
+
+
+
+// impi
+
+function getMonth(input){
+    var months = ['january' , 'february' , 'march' , 'april' , 'may' , 'june' , 'july' , 'august' , 'september' , 'october' , 'november' , 'december'];
+    
+    return arrayPosition(months , input);
+    
+}   
+
+function arrayPosition(months , input){
+    if(input > months.length){
+        return ;
+      
+      }
+      return months[input - 1];
 }
-else if(age < 18 && gender=='female'){
-    alert('eligible for girls school scholarship');
+
+
+
+function common(a){
+    if(getMonth(a)){
+        alert(`the ${a} month is ${getMonth(a)}`);
+    } else {
+        alert('this is false please enter correct number');
+    }
 }
-else if(age < 18 && gender=='male'){
-    alert('not eligible scholarship');
+
+var firstInput = prompt('enter the month number');
+common(firstInput);
+
+var names =['hari' , 'shyam' , 'ravi'];
+nameCommon(firstInput);
+function nameCommon(b){
+    if(names[b]){
+        alert(`name is ${names[b-1]}`);
+    }else{
+        alert(`there is no name in this postition`);
+    }
 }
+
+
+
+var secondInput = prompt(`enter the second number`);
+common(secondInput);
+
+
+
+
+
+
+
+
 
 
